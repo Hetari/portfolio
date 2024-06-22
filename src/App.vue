31ae71a40ebed5b76901af7b696c735408c979c7
Hetari <hetari4py@gmail.com>
2024-06-22 17:51:46 +0300

    fix bg filter

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="relative size-full overflow-clip">
-    <img
-      src="https://grainy-gradients.vercel.app/noise.svg"
-      alt="background"
-      class="pointer-events-none fixed inset-0 z-30 size-full object-cover object-center opacity-15"
-    />
+    <svg
+      class="pointer-events-none fixed inset-0 z-30 size-full object-cover object-center opacity-50"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <filter id="noise" x="0" y="0">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency="0.65"
+          numOctaves="3"
+          stitchTiles="stitch"
+        />
+        <feBlend mode="screen" />
+      </filter>
+      <rect class="size-full" filter="url(#noise)" opacity="0.3" />
+    </svg>
   </div>
 
   <Nav class="padding-x mx-auto max-w-[1920px]" />
