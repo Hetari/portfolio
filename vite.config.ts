91a956574dc998fc27d7bde79b243d612ee59f9c
Hetari <hetari4py@gmail.com>
2024-08-30 21:43:53 +0300

    add more SEO options

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import sitemap from 'vite-plugin-sitemap';
+import { robots } from 'vite-plugin-robots';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    sitemap({
+      hostname: 'https://hetari.github.io/',
+      basePath: 'portfolio',
+    }),
+    robots(),
+  ],
   resolve: {
     alias: {
       '@': '/src',
